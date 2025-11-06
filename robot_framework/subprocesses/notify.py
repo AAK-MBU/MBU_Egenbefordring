21adbfe879b752ecdc3b2f00362701a4356fc797
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-11-06 14:23:23 +0100

    fixed the url that is sent to plads

--- robot_framework/subprocesses/notify.py
+++ robot_framework/subprocesses/notify.py
@@ -12,7 +12,8 @@ def send_mail(orchestrator_connection: OrchestratorConnection):
     proc_args = json.loads(orchestrator_connection.process_arguments)
     receiver = proc_args["notification_email"]
     folder_dest = orchestrator_connection.folder_dest  # Manually set in process()
-    folder_url = config.SHAREPOINT_SITE_URL+config.DOCUMENT_LIBRARY+folder_dest
+
+    folder_url = config.SHAREPOINT_SITE_URL+"teams/"+config.SHAREPOINT_SITE_NAME+"/"+config.DOCUMENT_LIBRARY+"/"+config.DOCUMENT_FOLDER+"/"+folder_dest
     email_subject = "Robotten til egenbefordring er kørt"
     email_body = ('<p>Robotten til egenbefordring er nu kørt '
                   'og oversigten samt eventuelt relevante dokumenter '
